5f57881eea7ad7a92711fe62ea602ca1afc19e52
tez <thoroughgoodterry@gmail.com>
2025-07-29 15:37:10 +1000

    slightly modified wildcard

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: '/contact?*breed=',
+        disallow: '/contact*',
       },
     ],
     sitemap: `https://pickapooch.com.au/sitemap.xml`,
